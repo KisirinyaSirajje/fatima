fb8d09c0c0be3079fe9fc2f2d685847feeb7f1cb
kisirinya sirajje <kisirinyasiraje@gmail.com>
2025-09-30 19:51:25 +0300

    merge

--- FatimaSchoolManagement/obj/Debug/net8.0/FatimaSchoolManagement.AssemblyInfo.cs
+++ FatimaSchoolManagement/obj/Debug/net8.0/FatimaSchoolManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FatimaSchoolManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b4806005c99b306255169bfa5096210133fc25c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6cd5dce65a819a9c6320eb013efe82985feb5f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FatimaSchoolManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FatimaSchoolManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
